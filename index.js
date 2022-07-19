84afc3c66567ce9135ecace7e7a83f7b6e630869
jake dapper <jakedapper@gmail.com>
2022-07-19 15:39:36 -0500

    commit tree be994f7d8906e5af40778a7a2421167f504671bb

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+// const returnFirstTwoDrivers = function (driverArray) {
+//     let newArray = driverArray.slice(0,2)
+//     return newArray;
+// }
+
+// const returnLastTwoDrivers = function (driverArray) {
+//     return driverArray.slice(-2);
+// }
+
+// let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+
+// function createFareMultiplier(n) {
+//    return fare => fare * n
+//    }
+
+
+// function fareDoubler (fare) {
+//     return fare * 2;
+// }
+
+// function fareTripler (fare){
+//     return fare * 3;
+// }
+
+// function selectDifferentDrivers(driverArray, firstOrLast) {
+//     return firstOrLast(driverArray);
+// }
